a7dd9c130a58e8b90279e429448b88ce06d838ba
nessm <nessm@192.168.100.31>
2022-09-10 19:20:22 +0300

    Nesreen

--- src/project_cpit251/Project_CPIT251.java
+++ src/project_cpit251/Project_CPIT251.java
@@ -4,7 +4,8 @@ package project_cpit251;
 public class Project_CPIT251 {
 
     public static void main(String[] args) {
-       
+        
+        System.out.println("Hello, I am Nesreen Alghamdi");
     }
 
 }
