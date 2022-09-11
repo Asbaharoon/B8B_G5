a0c7a25dc757b580853d8a1d67a34fb4693f7ecc
shatha_alharbi <shatha3456alharbi@gmail.com>
2022-09-11 12:44:25 +0300

    shatha alharbi

--- src/project_cpit251/Project_CPIT251.java
+++ src/project_cpit251/Project_CPIT251.java
@@ -6,6 +6,8 @@ public class Project_CPIT251 {
     public static void main(String[] args) {
         
         System.out.println("Hello, I am Nesreen Alghamdi");
+        
+        System.out.println("Hiii I am shatha");
     }
 
 }
