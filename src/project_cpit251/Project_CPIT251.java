e5d1357458c694ab1d76c39a47cc1b5b2a497beb
Nesreen_Alghamdi <nessuf234@gmail.com>
2022-09-11 13:21:54 +0300

    NesreenAlghamdi

--- src/project_cpit251/Project_CPIT251.java
+++ src/project_cpit251/Project_CPIT251.java
@@ -5,7 +5,7 @@ public class Project_CPIT251 {
 
     public static void main(String[] args) {
         
-        System.out.println("Hello, I am Nesreen Alghamdi");
+        System.out.println("Hi, I am Nesreen Alghamdi");
         
         System.out.println("Hiii I am shatha");
     }
