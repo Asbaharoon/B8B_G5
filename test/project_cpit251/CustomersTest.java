3805eba18775767072a772509f6ce0a6d5f1e602
Shatha_Hamed_Almalki <shaz5141@gmail.com>
2022-11-06 00:56:06 +0300

    Shatha Hamed Almalki commit

---
+++ test/project_cpit251/CustomersTest.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project_cpit251;
+
+import java.util.ArrayList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author hp
+ */
+public class CustomersTest {
+    
+    public CustomersTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of searchForLawyer method, of class Customers.
+     */
+    
+    //Use assertEquals method to testing
+    @Test
+    public void testSearchForLawyer() {
+        System.out.println("searchForLawyer");
+        String info = "Ahmed Alghamdi";
+        ArrayList<Lawyer> list = new ArrayList<>();
+        Lawyer o=new Lawyer("Ahmed Alghamdi","0503472498","AhmedAlghamdi@gmail.com","Bachelor","Law","financial cases",150,30,1);
+        list.add(o);
+        Lawyer expResult = o;
+        Lawyer result = Customers.searchForLawyer(info, list);
+        assertEquals(expResult, result);
+      
+    }
+     
+    //Use NullObject method to testing
+     @Test
+    public void testNullObject() {
+        System.out.println("searchForLawyer");
+        String info = "Omar";
+        ArrayList<Lawyer> list = new ArrayList<>();
+        Lawyer o=new Lawyer("Ahmed Alghamdi","0503472498","AhmedAlghamdi@gmail.com","Bachelor","Law","financial cases",150,30,1);
+        list.add(o);
+        Lawyer result = Customers.searchForLawyer(info, list);
+        assertNull(result);
+      
+    }
+
+}
